35a13109a2a4f20c70eebe732c005d8ac826dab1
Justin Randall <justin@outofservice.org>
2024-03-12 17:57:52 -0400

    Bump the version to 0.0.6.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     # Basic package information:
     name="interactigen",
-    version="0.0.5",
+    version="0.0.6",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
 
